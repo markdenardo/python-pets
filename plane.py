ebeac54709b0c98ee6335710838a6e0562740d5b
MDN <mark.denardo@gmail.com>
2026-03-17 11:48:46 -0400

    Add Plane class and instantiate plane objects

---
+++ plane.py
@@ -0,0 +1,15 @@
+class Plane:
+    def __init__(self, name, sp, wt, acc, hnd):
+        self.name = name
+        self.sp = sp
+        self.wt = wt
+        self.acc = acc
+        self.hnd = hnd
+
+plane_1 = Plane("Boeing 747", 500, 4400, 10, 100)
+plane_2 = Plane("harrier", 200, 500, 5, 10)
+
+planes = [plane_1, plane_2]
+
+for x in planes:
+    print(x.name)
