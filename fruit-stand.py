135183083cd9c6699cc100a396f7979ecc390a2b
MDN <mark.denardo@gmail.com>
2026-03-17 11:56:26 -0400

    Add fruit stand game to randomly pick fruits

---
+++ fruit-stand.py
@@ -0,0 +1,8 @@
+import random
+answer=""
+basket=["bananas","apples","cherries"]
+while answer != "yes":
+    pick=random.choice(basket)
+    print(f"you got {pick}")
+    answer=input("do you want this?")
+print(f"enjoy your {pick}")
